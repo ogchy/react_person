c10fbad2ade89b9457d8fd0ed41683b290e8067f
ogchy <ogchyx@gmail.com>
2024-04-10 21:07:23 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,12 +3,11 @@ const Person = ({ person }) => {
   const maritalStatus = isMarried
     ? `Spouse: ${partnerName}`
     : 'I am not married';
-  const renderAge = age ? <p className="Person__age">Age: {age}</p> : null;
 
   return (
     <div className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {renderAge}
+      {age && <p className="Person__age">Age: {age}</p>}
       <p className="Person__marriedStatus">{maritalStatus}</p>
     </div>
   );
