bf1d08b66b5c77adebf579d176fe730f49e2d6cd
ogchy <ogchyx@gmail.com>
2024-04-10 20:44:25 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+const Person = ({ person }) => {
+  const { name, age, isMarried, partnerName } = person;
+  const maritalStatus = isMarried
+    ? `Spouse: ${partnerName}`
+    : 'I am not married';
+  const renderAge = age ? <p className="Person__age">Age: {age}</p> : null;
+
+  return (
+    <div className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {renderAge}
+      <p className="Person__marriedStatus">{maritalStatus}</p>
+    </div>
+  );
+};
+
+export default Person;
